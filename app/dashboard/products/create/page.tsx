c89c120eadfba05eb67b70958321d15a28249ba3
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2024-08-18 01:17:30 +0200

    In add product section: fixed images and layout but still need remove button

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+
+import { createProduct } from "@/app/actions";
 import { UploadDropzone } from "@/app/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -9,10 +11,28 @@ import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
+import { useFormState } from "react-dom";
+import {useForm} from "@conform-to/react"
+import { parseWithZod } from "@conform-to/zod";
+import { productSchema } from "@/lib/zodSchemas";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function ProductCreateRoute() {
+    const [images, setImages] =useState<string[]>([]);
+    const [lastResult, action] =useFormState(createProduct, undefined)
+    const [form, fields] = useForm({
+        lastResult,
+
+        onValidate({formData}){
+            return parseWithZod(formData, {schema: productSchema});
+        },
+
+        shouldValidate: 'onBlur',
+        shouldRevalidate: "onInput",
+    })
     return(
-        <form>
+        <form id={form.id} onSubmit={form.onSubmit} action={action}>
             <div className="flex items-center gap-4">
                 <Button variant="outline" size="icon" asChild>
                     <Link href="/dashboard/products">
@@ -31,22 +51,52 @@ export default function ProductCreateRoute() {
                     <div className="flex flex-col gap-6">
                         <div className="flex flex-col gap-3">
                             <Label>Name</Label>
-                            <Input type="text" className="w-full" placeholder="Product Name"/>
+                            <Input 
+                                type="text" 
+                                key={fields.name.key}
+                                name={fields.name.name}
+                                defaultValue={fields.name.initialValue}
+                                className="w-full" 
+                                placeholder="Product Name"
+                            />
+                            <p className="text-red-500">{fields.name.errors}</p>
                         </div>
                         <div className="flex flex-col gap-3">
                             <Label>Description</Label>
-                            <Textarea placeholder="Write your description here..."/>
+                            <Textarea 
+                                key={fields.description.key}
+                                name={fields.description.name}
+                                defaultValue={fields.description.initialValue}
+                                placeholder="Write your description here..."
+                            />
+                            <p className="text-red-500">{fields.description.errors}</p>
                         </div>
                         <div className="flex flex-col gap-3">
                             <Label>Price</Label>
-                            <Input type="number" placeholder="$34.90"/>
+                            <Input 
+                                key={fields.price.key}
+                                name={fields.price.name}
+                                defaultValue={fields.price.initialValue}
+                                type="number" 
+                                placeholder="$34.90"
+                            />
+                            <p className="text-red-500">{fields.price.errors}</p>
                         </div>
                         <div className="flex flex-col gap-3">
                             <Label>Featured Product</Label>
-                            <Switch/>
+                            <Switch 
+                                key={fields.isFeatured.key}
+                                name={fields.isFeatured.name}
+                                defaultValue={fields.isFeatured.initialValue}
+                            />
+                            <p className="text-red-500">{fields.isFeatured.errors}</p>
                         </div>
                         <div className="flex flex-col gap-3">
-                            <Select>
+                            <Select 
+                                key={fields.status.key}
+                                name={fields.status.name}
+                                defaultValue={fields.status.initialValue}
+                            >
                                 <SelectTrigger>
                                     <SelectValue placeholder="Select Status"/>
                                 </SelectTrigger>
@@ -56,16 +106,32 @@ export default function ProductCreateRoute() {
                                     <SelectItem value="archived">Archived</SelectItem>
                                 </SelectContent>
                             </Select>
+                            <p className="text-red-500">{fields.status.errors}</p>
                         </div>
                         <div className="flex flex-col gap-3">
                             <Label>Images</Label>
-                            <UploadDropzone 
+                            {images.length>0 ? (
+                                <div className="flex gap-5">
+                                    {images.map((image, index)=>(
+                                        <div key={index} className="relative w-[100px] h-[100px]">
+                                            <Image 
+                                                height={100} width={100} src={image} 
+                                                alt="Product Image" 
+                                                className="w-full h-full object-cover rounded-lg border"
+                                            />
+                                        </div>
+                                    ))}
+
+                                </div>
+                            ): (
+                                <UploadDropzone 
                                 endpoint="imageUploader"
                                 onClientUploadComplete={(res)=> {
-                                    alert('Uploaded Succesfully!');
+                                    setImages(res.map((r)=>r.url));
                                 }}
                                 onUploadError={()=>{alert("something went wrong");}}
-                            />
+                                />
+                            )}
                         </div>
                     </div>
                 </CardContent>
